ce9341a4857896dd05970f6aff7b71b9e8247dae
aruwksc <240966@astanait.edu.kz>
2026-02-12 22:47:28 +0500

    dd

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/habitflow';
+    const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
     const conn = await mongoose.connect(uri);
     console.log(` MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
